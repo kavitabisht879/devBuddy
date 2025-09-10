c0f6f1ac152cf4b7c9a4bc9933031d51bb77d778
kavitabisht11 <kavita.bisht@pharynxai.com>
2025-09-10 13:29:25 +0530

    connection apis created

--- src/routes/user.js
+++ src/routes/user.js
@@ -1,8 +1,8 @@
 const express = require("express");
 const { userAuth } = require("../middlewares/auth");
-const  ConnectionRequest  = require("../models/connectionRequest");
+const ConnectionRequest = require("../models/connectionRequest");
 const userRouter = express.Router();
-
+const USER_SAFE_DATA= "firstName lastName photoUrl age gender about skills";
 userRouter.get("/user/requests/received", userAuth, async (req, res) => {
   try {
     const loggedInUser = req.user;
@@ -22,5 +22,43 @@ userRouter.get("/user/requests/received", userAuth, async (req, res) => {
     res.status(404).send("Error: " + error.message);
   }
 });
+
+userRouter.get("/user/connections", userAuth, async (req, res) => {
+  try {
+    const loggedInUser = req.user;
+    const connectionRequests = await ConnectionRequest.find({
+      $or: [
+        { toUserId: loggedInUser._id, status: "accepted" },
+        { fromUserId: loggedInUser._id, status: "accepted" },
+      ],
+    }).populate("fromUserId", USER_SAFE_DATA)
+      .populate("toUserId", USER_SAFE_DATA);
+    console.log("connectionRequests", connectionRequests);
+    const data = connectionRequests.map((row) => {
+      if (row.fromUserId._id.toString() === loggedInUser._id.toString()) {
+        return row.toUserId;
+      }
+      return row.fromUserId;
+    });
+    res.json({ data });
+  } catch (error) {
+    res.status(404).send({ message: error.message });
+  }
+});
+
+
+userRouter.get("/feed" , userAuth , async(req,res)=>{
+  try{
+
+  }catch(error){
+    res.status(404).send({ message: error.message });
+  }
+})
+
+
+
+
 module.exports = { userRouter };
-// 34
+
+
+// -1-02-31
